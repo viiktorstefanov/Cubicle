f3fcff455bb66634a4cca3952970b0db2ae850b9
viktor0110 <viktor-stefanov@mail.bg>
2023-08-15 21:13:38 +0300

    page views added.

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const port = 3000;
 const hbs = require('express-handlebars');
-hbs.create({
+const handlebars = hbs.create({
     extname: '.hbs'
 });
 
+const homeController = require('./controllers/homeController');
+const aboutController = require('./controllers/aboutController');
+const detailsController = require('./controllers/detailsController');
+const createController = require('./controllers/createController');
+const defaultController = require('./controllers/defaultController');
+
 const app = express();
-app.engine('.hbs', hbs.engine);
+app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
+
+app.use(homeController);
+app.use('/about', aboutController);
+app.use('/create', createController);
+app.use('/details/:id', detailsController);
+app.all('*', defaultController);
 
 app.listen(port);
