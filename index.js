1cee2a0deda7b430290e6c495107442aafcce752
viktor0110 <viktor-stefanov@mail.bg>
2023-09-07 15:31:53 +0300

    edit view removed error notification.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const expressConfig = require('./config/express');
 const routesConfig = require('./config/routes');
 const databaseConfig = require('./config/database');
-const port = 3000;
+const port = 5000;
 
 start();
 
